05a922bf12dbb7914fe8b681827580c7838fd998
mmoscare <michaelmoscare@gmail.com>
2025-02-16 16:26:17 -0500

    Working

---
+++ next.config.mjs
@@ -0,0 +1,20 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
+import dotenv from "dotenv";
+
+// Load environment variables from .env.local
+dotenv.config({ path: ".env.local" });
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  env: {
+    CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    DATABASE_URL: process.env.DATABASE_URL,
+  },
+};
+
+export default nextConfig;
